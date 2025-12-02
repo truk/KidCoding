61a95273f50f183feab120f4f1d71bd6db86afc2
Kurt Eldridge <kilofoxtrotecho@gmail.com>
2025-12-01 21:52:55 -0500

    Delete monsters directory

--- monsters/README.md
+++
@@ -1,7 +0,0 @@
-## Monsters in React
-
-This project is to slowly build up a monster game in React
-
-It starts with a very minimal project and adds features incrementally
-
-It is developed in partnership with my boys and inspired by one of their favorite games
--- monsters/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders a button to begin a battle', () => {
-  const { getByText } = render(<App />);
-  const buttonElement = getByText(/battle/i);
-  expect(buttonElement).toBeInTheDocument();
-});
